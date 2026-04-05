95432d0cd48538605545ca7f559ffd614aee2145
Osipov0609 <stas-osipov-2004@mail.ru>
2026-04-05 17:31:27 +0400

    Start

--- server/server.js
+++ server/server.js
@@ -1,18 +1,76 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const fs = require('fs')
-const PORT = 5000
-const bodyParser = require("body-parser");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const apartments = require("./apartament.json");
+const house = require("./house.json");
+const lot = require('./lot.json')
+const realtor = require('./realtor.json')
+const axios = require('axios')
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended : true }))
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const imagesPath = path.join(__dirname, '../front/src/images');
+app.use('/images', express.static(imagesPath));
+
+app.get('/apartments', (req, res) => {
+    const { rooms, building, furnishing, repair, type } = req.query;
+
+    let filtered = apartments.filter(a => {
+        return (!rooms || a.rooms === rooms) &&
+            (!building || a.building === building) &&
+            (!furnishing || a.furnishing === furnishing) &&
+            (!repair || a.repair === repair) &&
+            (!type || a.type === type);
+    });
+
+    res.json(filtered);
+});
+
+app.get('/houses', (req, res) => {
+    const { rooms, type, propertyType, building, furnishing, repair } = req.query
+    let filtreted = house.filter(a => {
+
+        return (!rooms || a.rooms === rooms) &&
+            (!propertyType || a.propertyType === propertyType) &&
+            (!building || a.building === building) &&
+            (!furnishing || a.furnishing === furnishing) &&
+            (!repair || a.repair === repair) &&
+            (!type || a.type === type);
+    })
+    res.json(filtreted)
+})
+
+app.get('/lot',(req,res) => {
+   res.json(lot);
+})
+app.get('/realtor', (req,res) => {
+    res.json(realtor)
+})
 
 
-
+const TOKEN = "8756676669:AAHkKwSpy5NQLLoL03Slt9D_nlRgH6KVOoo";
+const CHAT_ID = '939294728';
+app.post('/li', async (req, res) => {
+    const { message } = req.body;
+    
+    try {
+        await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+            chat_id: CHAT_ID,
+            text: message
+        });
+        console.log('Հաղորդագրությունը ուղարկվեց Telegram-ին');
+        res.status(200).send('Հաջողություն');
+    } catch (error) {
+        console.log('Սխալ Telegram-ի հետ կապի ժամանակ');
+        res.status(500).send('Սխալ');
+    }
+});
 
 
 app.listen(PORT, () => {
-    console.log('server runinig http://localhost:5000');
-})
+    console.log(`Server is running on http://localhost:5000`);
+});
